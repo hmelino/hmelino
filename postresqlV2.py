a5a3f640f64fccb2a1599fd1d9bdba3b94a54420
Matus Hmelar <hmelino.github@gmail.com>
2020-04-01 15:30:29 +0100

    New function 'total daily sales'

--- postresqlV2.py
+++ postresqlV2.py
@@ -4,6 +4,8 @@ import csv
 import codecs
 import time
 import sys
+import difflib
+import matplotlib.pyplot as plt
 
 db=psycopg2.connect(dbname='thesales',user='postgres',host='localhost', password='nopasswordhere' )
 cur=db.cursor()
@@ -26,7 +28,6 @@ def getTableName(readerObject):
     else:
         print('unknow file')
         sys.exit()
-
 
 path='E:\PythonCoding\Reports'
 path2='E:\PythonCoding\Reports2'
@@ -73,7 +74,55 @@ def loadFolderCopy(path):
     db.commit()
 
 #loadFolderSlow(path)
-loadFolderCopy(path2)
+#loadFolderCopy(path2)
+
+
+
+
+
+
+
+def totalSalesPerDay(item):
+    ''' Get list of daily total sales of item per whole date range  '''
+    def findCorrectItem(item):
+        cur.execute(""" SELECT DISTINCT description from sales2;""")
+        itemsList=[v[0] for v in cur]
+        try:
+            return difflib.get_close_matches(item,itemsList)[0]
+        except IndexError:
+            print(f'Didnt find {item}')
+            sys.exit()
+
+    def searchForTotals(item):
+        cur.execute(f""" SELECT 
+        sales2.description as name,
+        to_date(payments2.paymenttime,'DD Mon YYYY') as date,
+        sum(cast(sales2.quantity as int))
+        FROM sales2 
+        INNER JOIN payments2 ON payments2.checkitemid = sales2.checkitemid 
+        WHERE sales2.description ='{item}'
+        GROUP BY date,name
+        ORDER BY date
+        """)
+        result=[v[2] for v in cur]
+        if result:
+            return result
+        else:
+            return False
+    
+    r = searchForTotals(item)
+    if r is False:
+        r = searchForTotals(findCorrectItem(item))
+    return r
+
+
+sales=totalSalesPerDay('Cappucino')
+plt.plot(sales)
+
+plt.show()
+
+
 
 db.close()
+
 pass
